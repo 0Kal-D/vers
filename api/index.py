c119bde17a213dc93dd2dccdd19ef5fa96d0d80e
Kal_D <88226646+0Kal-D@users.noreply.github.com>
2024-04-09 11:27:15 +0300

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from http.server import BaseHTTPRequestHandler
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, Kal"
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        response = app.test_client().get("/")
+        self.wfile.write(response.data)
+        return
